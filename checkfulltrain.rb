9b8029c554309c5e52730a7f32b0146296a0cc04
Yann PETITJEAN <yann120@hotmail.fr>
2019-05-11 07:08:33 +0000

    push before offline, work on the hash of requested trips

--- checkfulltrain.rb
+++ checkfulltrain.rb
@@ -9,7 +9,7 @@ def csv_to_array(string)
     csv.collect { |record| Hash[*fields.zip(record).flatten ] } 
 end
 
-def trainline_query
+def trainline_query()
 	departure_station = ARGV[0]
 	arrival_station = ARGV[1]
 	from_date = ARGV[2]
@@ -23,11 +23,30 @@ def tgvmax_checker(trips)
 	trips.each do |trip|
 		return trip if trip["price"] == "0"
 	end
-	return NULL
+	return false
 end
+
+def loop_checker(trips) #work in progress
+	free_trip = tgvmax_checker(trips)
+	while (!free_trip)
+		puts "no free tgvmax trips :("
+		sleep(1.hour)
+		free_trip = tgvmax_checker(trips)
+	end
+end
+
+trips_to_search = []
+trip1 = {:departure_station => "Toulouse" , :arrival_station => "Bordeaux", :from_date => "14/05/2019 08:00", :to_date => "14/05/2019 21:00"}
+trips_to_search.push(trip1)
+trip2 = {:departure_station => ARGV[0] , :arrival_station => ARGV[1], :from_date => ARGV[2], :to_date => ARGV[3]}
+trips_to_search.push(trip2)
+
 
 query_result = trainline_query
 trips = csv_to_array(query_result)
 free_trip = tgvmax_checker(trips)
-puts free_trip unless free_trip.nil?
-
+if free_trip
+	puts free_trip
+else
+	puts "aucun train dispo"
+end
