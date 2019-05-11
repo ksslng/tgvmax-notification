64287227420cd7ec5ab17654a077b77c498ca9ca
Yann PETITJEAN <yann120@hotmail.fr>
2019-05-11 09:02:44 +0000

    iteration on the array of multiples trips_to_search is working fine, I should now check how to do store all the trips to search in a JSON file" "

--- checkfulltrain.rb
+++ checkfulltrain.rb
@@ -9,11 +9,11 @@ def csv_to_array(string)
     csv.collect { |record| Hash[*fields.zip(record).flatten ] } 
 end
 
-def trainline_query()
-	departure_station = ARGV[0]
-	arrival_station = ARGV[1]
-	from_date = ARGV[2]
-	to_date = ARGV[3]
+def trainline_query(requested_trip)
+	departure_station = requested_trip[:departure_station]
+	arrival_station = requested_trip[:arrival_station]
+	from_date = requested_trip[:from_date]
+	to_date = requested_trip[:to_date]
 
 	query = "python3 main.py \'#{departure_station}\' \'#{arrival_station}\' \'#{from_date}\' \'#{to_date}\'"
 	result = `#{query}`
@@ -26,12 +26,29 @@ def tgvmax_checker(trips)
 	return false
 end
 
-def loop_checker(trips) #work in progress
-	free_trip = tgvmax_checker(trips)
-	while (!free_trip)
-		puts "no free tgvmax trips :("
-		sleep(1.hour)
-		free_trip = tgvmax_checker(trips)
+def search_loop(trips_to_search) #work in progress
+	while(!trips_to_search.empty?) do
+		trips_to_search.each do |trip_to_search|
+			query_result = trainline_query(trip_to_search)
+			search_results = csv_to_array(query_result)
+			if (free_trip = tgvmax_checker(search_results))
+				#Notification action
+				puts "Le train suivant est disponible : "
+				puts free_trip
+				#puts "delete index #{index}"
+				trips_to_search.delete(trip_to_search)
+				#Delete trip_to_search from array
+			else
+				puts "Aucun train dispo de #{trip_to_search[:departure_station]} a #{trip_to_search[:arrival_station]} entre le #{trip_to_search[:from_date]} et #{trip_to_search[:to_date]}"
+			end
+		end
+		if (!trips_to_search.empty?)
+			puts "Prochaine recherche dans une heure"
+			sleep(10)
+			#sleep(3600)
+		else
+			puts "Toutes les recherches ont permis de trouver des TGVMAX disponible"
+		end
 	end
 end
 
@@ -41,12 +58,4 @@ trips_to_search.push(trip1)
 trip2 = {:departure_station => ARGV[0] , :arrival_station => ARGV[1], :from_date => ARGV[2], :to_date => ARGV[3]}
 trips_to_search.push(trip2)
 
-
-query_result = trainline_query
-trips = csv_to_array(query_result)
-free_trip = tgvmax_checker(trips)
-if free_trip
-	puts free_trip
-else
-	puts "aucun train dispo"
-end
+search_loop(trips_to_search)
