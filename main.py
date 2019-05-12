9161c65ebd42645e20b861c7f5481252c8efd380
Yann PETITJEAN <yann120@hotmail.fr>
2019-05-12 13:39:53 +0000

    small changes on python script

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
+# Code from Tducret (https://ww.tducret.com/) that parse trainline in CSV 
+# More informations on his project here : https://github.com/tducret/trainline-python
+
 import trainline
 import sys
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 if len(sys.argv) != 5:
     sys.exit("usage: python3 main.py departure_station arrival_station from_date to_date\nexemple : python3 main.py Paris Nice '10/05/2019 08:00' '10/05/2019 21:00'")
 
-Yann = trainline.Passenger(birthdate="04/07/1994")
-Yann.add_special_card(trainline.TGVMAX, 'TGVMAXKEY')
+User = trainline.Passenger(birthdate=os.getenv("BIRTHDATE"))
+User.add_special_card(trainline.TGVMAX, os.getenv("TGVMAX"))
 
 results = trainline.search(
-	passengers=[Yann],
+	passengers=[User],
 	departure_station=sys.argv[1],
 	arrival_station=sys.argv[2],
 	from_date=sys.argv[3],
